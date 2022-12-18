f503f5de8d1d5380fb8c39daec9794f1dd72dad6
league <league@imac-18.attlocal.net>
2022-12-18 11:25:06 -0800

    Monster and years.

--- src/_01_else_if/_5_own_adventure/OwnAdventure.java
+++ src/_01_else_if/_5_own_adventure/OwnAdventure.java
@@ -10,11 +10,11 @@ public class OwnAdventure {
 		if(begin.equalsIgnoreCase("CHECK")) {
 			String checked = JOptionPane.showInputDialog("You carefully approch the glass door and peer outside. First you see nothing. Then a horrifing face slams against the glass looking at you with two glowing yellow eyes. \n The face begins to smile like a maniac showing sharp yellowed teeth. You scramble into the kitchen searching for anything to defend yourself. If you grab the knife, type KNIFE. If you grab the sponge, type SPONGE. If you grab the frying pan, type PAN. If you give up, type anything else.");
 			if(checked.equalsIgnoreCase("KNIFE")) {
-				JOptionPane.showInputDialog("Under construction.");
+				JOptionPane.showInputDialog("The monster charged into the room towards you but you stand your ground. As it is about to strike, you drive two knifes into the eyes of the monster. It slumpes to the ground, bleeding from the eyes.\n                                           YOU WIN!");
 			if(checked.equalsIgnoreCase("PAN")){
-				JOptionPane.showInputDialog("Under construction.");
+				JOptionPane.showInputDialog("The monster raced into the room. You managed to get behind it and hit it in the back of the head. You hear a sickining crack and the beast slumps to the ground.\n                                           YOU WIN!");
 			if(checked.equalsIgnoreCase("SPONGE")){
-				JOptionPane.showInputDialog("Under construction.");
+				JOptionPane.showInputDialog("It's a sponge. What did you think it would do?\n                                           THE MONSTER GETS YOU.");
 			}
 			}
 		}else {
